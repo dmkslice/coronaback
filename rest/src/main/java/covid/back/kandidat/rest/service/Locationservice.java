16d654b0f3e35f569c6cbf0d7d2a9bbddb3b5de8
henkewahlstrom <henrik@ewhale.se>
2020-04-24 13:54:57 +0200

    Location Querys

--- rest/src/main/java/covid/back/kandidat/rest/service/Locationservice.java
+++ rest/src/main/java/covid/back/kandidat/rest/service/Locationservice.java
@@ -24,7 +24,7 @@ public class Locationservice {
     int tiredness = 0;
 	
 	
-	public ArrayList<Location> getAllLocations(String timestamp){
+    public ArrayList<Location> getAllLocations(String timestamp){
         ArrayList<Location> locationList = new ArrayList<Location>();
         int symptomLocationID = 0;
         try{  
@@ -32,7 +32,7 @@ public class Locationservice {
             Connection con=DriverManager.getConnection(URL, databaseUsername, databasePassword);  
             //here DatabaseCoronaApp is the database name, root is username and password  
             java.sql.Statement stmt= con.createStatement();  
-            ResultSet rs=((java.sql.Statement) stmt).executeQuery("select * from SymptomLocationsNew");  
+            ResultSet rs=((java.sql.Statement) stmt).executeQuery("select * from SymptomLocation");  
             while(rs.next()) {
                 symptomLocationID = rs.getInt(1);
                 longitude = rs.getString(2);
@@ -62,13 +62,13 @@ public class Locationservice {
     }
 	
 	
-	
 		
 	public ArrayList<Location> getUserLocation(String email){
         ArrayList<Location> locationList = new ArrayList<Location>();
         int userID = 0;
         List<Integer> locationIDList = new ArrayList<>();
         int symptomLocationID = 0;
+        int rowsOfLocationID = 0;
         try{  
             Class.forName("com.mysql.cj.jdbc.Driver");  
             Connection con=DriverManager.getConnection(URL, databaseUsername, databasePassword);  
@@ -78,35 +78,40 @@ public class Locationservice {
             while(rs.next()) {
                 userID= rs.getInt(1);
             } 
+            ResultSet num= stmt.executeQuery("select COUNT(*) from User_Location where User_Id ='"+userID+"'");
+            while(num.next()) {  
+            	rowsOfLocationID=num.getInt(1);
+                System.out.println(rowsOfLocationID);
+            }
             ResultSet findLocationID = stmt.executeQuery("select SymptomLocation_Id from User_Location where User_Id ='"+userID+"'");
-            findLocationID.last();
-            int rowsOfLocationID = findLocationID.getRow();
-            if (findLocationID.next()) {
-                for (int i = 0; i<rowsOfLocationID; i++){
+            
+                for (int i = 1; i<=rowsOfLocationID; i++){
+                    while (findLocationID.next()) {
                     locationIDList.add(findLocationID.getInt(i));
+                    System.out.println(locationIDList);
                 }
             }
             for (int i = 0; i<rowsOfLocationID; i++){
-                ResultSet findLocations = stmt.executeQuery("select * from SymptomLocation where ='"+locationIDList.get(i)+"'");
+                ResultSet findLocations = stmt.executeQuery("select * from SymptomLocation where Id='"+locationIDList.get(i)+"'");
                 while(findLocations.next()) {
-                symptomLocationID = rs.getInt(1);
-                longitude = rs.getString(2);
-                latitude = rs.getString(3);
-                userTimestamp = rs.getString(4);
-                LocalDateTime convertedTimestamp = LocalDateTime.parse(userTimestamp);
-                userDate = rs.getString(5);
-                LocalDate convertedDate = LocalDate.parse(userDate);
-                headache = rs.getInt(6);
-                nasalCongestion = rs.getInt(7);
-                soreThroat = rs.getInt(8);
-                breathingDifficulties = rs.getInt(9);
-                cough = rs.getInt(10);
-                runnyNose = rs.getInt(11);
-                fever = rs.getInt(12);
-                diarrhea = rs.getInt(13);
-                tiredness = rs.getInt(14);
-                Location location = new Location (longitude, latitude, convertedDate, symptomLocationID, headache, nasalCongestion, soreThroat, breathingDifficulties, cough, runnyNose, fever, diarrhea, tiredness, convertedTimestamp);
-                locationList.add(location);
+                    symptomLocationID = findLocations.getInt(1);
+                    longitude = findLocations.getString(2);
+                    latitude = findLocations.getString(3);
+                    userTimestamp = findLocations.getString(4);
+                    LocalDateTime convertedTimestamp = LocalDateTime.parse(userTimestamp);
+                    userDate = findLocations.getString(5);
+                    LocalDate convertedDate = LocalDate.parse(userDate);
+                    headache = findLocations.getInt(6);
+                    nasalCongestion = findLocations.getInt(7);
+                    soreThroat = findLocations.getInt(8);
+                    breathingDifficulties = findLocations.getInt(9);
+                    cough = findLocations.getInt(10);
+                    runnyNose = findLocations.getInt(11);
+                    fever = findLocations.getInt(12);
+                    diarrhea = findLocations.getInt(13);
+                    tiredness = findLocations.getInt(14);
+                    Location location = new Location (longitude, latitude, convertedDate, symptomLocationID, headache, nasalCongestion, soreThroat, breathingDifficulties, cough, runnyNose, fever, diarrhea, tiredness, convertedTimestamp);
+                    locationList.add(location);
                 }
             }
             
@@ -117,6 +122,7 @@ public class Locationservice {
         }  
         return locationList;
     }
+
 	
 	public boolean createUserLocation(String email, String longitude, String latitude, String userTimestamp, String userDate, int headache, int nasalCongestion, int soreThroat, int breathingDifficulties, int cough, int runnyNose, int fever, int diarrhea, int tiredness){
         boolean validUserCreated = false;
@@ -129,16 +135,19 @@ public class Locationservice {
             Connection con=DriverManager.getConnection(URL, databaseUsername, databasePassword);  
             //here DatabaseCoronaApp is the database name, root is username and password  
             Statement stmt=con.createStatement(); 
-            ResultSet num= stmt.executeQuery("select COUNT(*) from SymptomLocationNew");
+            ResultSet num= stmt.executeQuery("select COUNT(*) from SymptomLocation");
             while(num.next()) {  
             	SymptomLocationID=num.getInt(1);
+                System.out.println("Hej");
             }
             SymptomLocationID++;
             ResultSet rs=stmt.executeQuery("select ID from User where email='" + email + "'");  
+            System.out.println("Tja");
             if (rs.next()) {
                 userID= rs.getInt(1);
-                stmt.executeUpdate("INSERT into SymptomLocationNew values('"+SymptomLocationID +"','"+longitude+"','"+latitude+"','"+userTimestamp+"','"+userDate+"','"+headache+"','"+nasalCongestion+"','"+soreThroat+"','"+breathingDifficulties+"','"+cough+"','"+runnyNose+"','"+fever+"','"+diarrhea+"','"+tiredness+"')");
-                
+                System.out.println(userID);
+                stmt.executeUpdate("INSERT into SymptomLocation values('"+SymptomLocationID +"','"+longitude+"','"+latitude+"','"+userTimestamp+"','"+userDate+"','"+headache+"','"+nasalCongestion+"','"+soreThroat+"','"+breathingDifficulties+"','"+cough+"','"+runnyNose+"','"+fever+"','"+diarrhea+"','"+tiredness+"')");
+                stmt.executeUpdate("INSERT into User_Location values('"+userID+"','"+SymptomLocationID+"')");
                 validUserCreated = true;
             } 
             con.close(); 
@@ -164,7 +173,7 @@ public List<Location> updateuserLocations(List<Location> userslocations){
 	
 }
 
-public List<Location> removeLocations(List<Location> removelocations){
+public List<Location> removeUserLocations(List<Location> removelocations){
 	
 	
 	return null;
